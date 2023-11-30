d5bdc2b5cae7f3d8b9af924d2a739cf5c0a87035
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-11-30 12:00:23 -0800

    adding files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name___ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
